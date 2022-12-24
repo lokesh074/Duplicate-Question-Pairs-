925282c2ef4cb0a276d495610187a77bf255c393
Lokesh Sharma <91384498+lokesh074@users.noreply.github.com>
2022-12-24 16:06:22 +0530

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,129 @@
+import re
+from bs4 import BeautifulSoup
+import distance
+import pickle
+import numpy as np
+
+cv = pickle.load(open('CountVector.pkl','rb'))
+
+stopwords_english = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their',
+'theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had''having''do''does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']
+
+def test_common_words(q1,q2):
+    w1 = set(map(lambda word: word.lower().strip(), q1.split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), q2.split(" ")))
+    return len(w1 & w2)
+
+def test_total_words(q1,q2):
+    w1 = set(map(lambda word: word.lower().strip(), q1.split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), q2.split(" ")))
+    return (len(w1) + len(w2))
+
+
+def test_fetch_token_features(q1, q2):
+    SAFE_DIV = 0.0001
+
+    STOP_WORDS = stopwords_english
+
+    token_features = [0.0] * 8
+
+    # Converting the Sentence into Tokens:
+    q1_tokens = q1.split()
+    q2_tokens = q2.split()
+
+    if len(q1_tokens) == 0 or len(q2_tokens) == 0:
+        return token_features
+
+    # Get the non-stopwords in Questions
+    q1_words = set([word for word in q1_tokens if word not in STOP_WORDS])
+    q2_words = set([word for word in q2_tokens if word not in STOP_WORDS])
+
+    # Get the stopwords in Questions
+    q1_stops = set([word for word in q1_tokens if word in STOP_WORDS])
+    q2_stops = set([word for word in q2_tokens if word in STOP_WORDS])
+
+    # Get the common non-stopwords from Question pair
+    common_word_count = len(q1_words.intersection(q2_words))
+
+    # Get the common stopwords from Question pair
+    common_stop_count = len(q1_stops.intersection(q2_stops))
+
+    # Get the common Tokens from Question pair
+    common_token_count = len(set(q1_tokens).intersection(set(q2_tokens)))
+
+    token_features[0] = common_word_count / (min(len(q1_words), len(q2_words)) + SAFE_DIV)
+    token_features[1] = common_word_count / (max(len(q1_words), len(q2_words)) + SAFE_DIV)
+    token_features[2] = common_stop_count / (min(len(q1_stops), len(q2_stops)) + SAFE_DIV)
+    token_features[3] = common_stop_count / (max(len(q1_stops), len(q2_stops)) + SAFE_DIV)
+    token_features[4] = common_token_count / (min(len(q1_tokens), len(q2_tokens)) + SAFE_DIV)
+    token_features[5] = common_token_count / (max(len(q1_tokens), len(q2_tokens)) + SAFE_DIV)
+
+    # Last word of both question is same or not
+    token_features[6] = int(q1_tokens[-1] == q2_tokens[-1])
+
+    # First word of both question is same or not
+    token_features[7] = int(q1_tokens[0] == q2_tokens[0])
+
+    return token_features
+
+
+def test_fetch_length_features(q1, q2):
+    length_features = [0.0] * 3
+
+    # Converting the Sentence into Tokens:
+    q1_tokens = q1.split()
+    q2_tokens = q2.split()
+
+    if len(q1_tokens) == 0 or len(q2_tokens) == 0:
+        return length_features
+
+    # Absolute length features
+    length_features[0] = abs(len(q1_tokens) - len(q2_tokens))
+
+    # Average Token Length of both Questions
+    length_features[1] = (len(q1_tokens) + len(q2_tokens)) / 2
+
+    strs = list(distance.lcsubstrings(q1, q2))
+    length_features[2] = len(strs[0]) / (min(len(q1), len(q2)) + 1)
+
+    return length_features
+
+
+
+
+
+def query_point_creator(q1, q2):
+    input_query = []
+
+    # preprocess
+   # q1 = preprocess(q1)
+    #q2 = preprocess(q2)
+
+    # fetch basic features
+    input_query.append(len(q1))
+    input_query.append(len(q2))
+
+    input_query.append(len(q1.split(" ")))
+    input_query.append(len(q2.split(" ")))
+
+    input_query.append(test_common_words(q1, q2))
+    input_query.append(test_total_words(q1, q2))
+    input_query.append(round(test_common_words(q1, q2) / test_total_words(q1, q2), 2))
+
+    # fetch token features
+    token_features = test_fetch_token_features(q1, q2)
+    input_query.extend(token_features)
+
+    # fetch length based features
+    length_features = test_fetch_length_features(q1, q2)
+    input_query.extend(length_features)
+
+
+    # bow feature for q1
+    q1_bow = cv.transform([q1]).toarray()
+
+    # bow feature for q2
+    q2_bow = cv.transform([q2]).toarray()
+
+    return np.hstack((np.array(input_query).reshape(1, 18), q1_bow, q2_bow))
+
